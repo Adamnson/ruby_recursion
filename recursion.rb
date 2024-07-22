dcd1890084b44dc918913965cc6e5b04debb9d44
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-07-22 20:41:49 +0530

    compiled print statements at EOF

--- recursion.rb
+++ recursion.rb
@@ -5,27 +5,22 @@ def collatz(n, step = 0)
   (n % 2 == 0) ? collatz(n/2, step + 1) : collatz( 3*n + 1, step + 1)
 end
 
-# puts collatz(27) # 111
-
 def factorial(n)
   return 1 if n.eql?(1)
   n * factorial(n - 1)
 end
 
-# puts factorial(5) # 120
-
-# def check_palindrome(s)
-#   if s.length >= 2
-#     if s[0] == s[-1] ? true : false
-#   elsif s.length == 1 || s.length == 0
-#     true
-#   end
-#   check_palindrome(s[1,s.length-2])
-# end
-
-# puts check_palindrome("malayalam")
-# puts check_palindrome("jarrasj")
-
+def check_palindrome(string)
+  if string.length == 1 || string.length == 0
+    true
+  else
+    if string[0] == string[-1]
+      palindrome(string[1..-2])
+    else
+      false
+    end
+  end
+end
 
 def beer_bottles(n)
   if n.eql?(0)
@@ -35,8 +30,6 @@ def beer_bottles(n)
   puts "#{n} bottle#{n>1 ? 's' : '' } of beer on the wall"
   beer_bottles(n-1)
 end
-
-# beer_bottles(7)
 
 def fibonacci_val(n)
   return nil unless n.positive?
@@ -50,12 +43,6 @@ def fibonacci_val(n)
   end
 end
 
-
-# puts fibonacci_val(0) #nil
-# puts fibonacci_val(2) #1
-# puts fibonacci_val(7) #8
-# puts fibonacci_val(15) #377
-
 def flatten_array(arr, ret_val = [])
   arr.each do |e|
     if e.is_a?(Array)
@@ -67,10 +54,6 @@ def flatten_array(arr, ret_val = [])
   ret_val
 end
 
-# puts flatten_array( [[1, [8, 9]], [3, 4]] )
-# puts flatten_array( [[1, 2], [3, 4]] )
-
-# binding.pry
 def integer_to_roman(number, result = "")
   roman_mapping = {
   1000 => "M",
@@ -94,9 +77,6 @@ def integer_to_roman(number, result = "")
     return integer_to_roman( modulus, result) if quotient > 0
   end
 end
-
-# puts integer_to_roman(14)
-
 
 def roman_to_integer(str, result = 0)
   roman_mapping = {
@@ -124,5 +104,18 @@ def roman_to_integer(str, result = 0)
   end
 end
 
+
+puts collatz(27) # 111
+puts factorial(5) # 120
+puts check_palindrome("malayalam")
+puts check_palindrome("jarrasj")
+beer_bottles(7)
+puts fibonacci_val(0) #nil
+puts fibonacci_val(2) #1
+puts fibonacci_val(7) #8
+puts fibonacci_val(15) #377
+puts flatten_array( [[1, [8, 9]], [3, 4]] )
+puts flatten_array( [[1, 2], [3, 4]] )
+puts integer_to_roman(14)
 puts roman_to_integer("IV")
 puts roman_to_integer("MCLXCVI")
